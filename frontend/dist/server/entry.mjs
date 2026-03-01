924b321a8978949f7b334498318cb79fea13af06
pabloeg11 <pabloescg4@gmail.com>
2026-03-01 22:54:50 +0000

    pagina completa con dominio critixreviews.es

--- frontend/dist/server/entry.mjs
+++ frontend/dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_BRa8g76m.mjs';
-import { manifest } from './manifest_COAzwIla.mjs';
+import { manifest } from './manifest_B9TWDBEw.mjs';
 
 const serverIslandMap = new Map();;
 
